c85879ccd641f922126ec3a454f55616b28b82f4
Dhruv Pahwa <dhruvpahwa02@gmail.com>
2025-04-01 21:52:44 +0530

    first commit

--- src/Service/GlobalApi.jsx
+++ src/Service/GlobalApi.jsx
@@ -2,39 +2,41 @@ import axios from "axios";
 
 const BASE_URL = "https://places.googleapis.com/v1/places:searchText";
 
-const getHeaders = (fieldMask) => ({
+const configPlace = {
   headers: {
     "Content-Type": "application/json",
     "X-Goog-Api-Key": import.meta.env.VITE_GOOGLE_MAP_API_KEY,
-    "X-Goog-FieldMask": fieldMask.join(","), // Field masks must be comma-separated
+    "X-Goog-FieldMask": [
+      "places.id",
+      "places.name",
+      "places.displayName",
+      "places.formattedAddress",
+      "places.photos",
+      "places.googleMapsUri",
+      "places.location",
+      "places.priceLevel",
+      "places.rating",
+    ],
   },
-});
-
-export const PHOTO_URL = (photoReference) =>
-  `https://places.googleapis.com/v1/${photoReference}/media?maxHeightPx=1000&key=${import.meta.env.VITE_GOOGLE_MAP_API_KEY}`;
-
-export const getPlaceDetails = (data) => {
-  const fieldMask = [
-    "id",
-    "name",
-    "displayName",
-    "formattedAddress",
-    "photos",
-    "googleMapsUri",
-    "location",
-    "priceLevel",
-    "rating",
-  ];
-  return axios.post(BASE_URL, data, getHeaders(fieldMask));
+};
+const configCity = {
+  headers: {
+    "Content-Type": "application/json",
+    "X-Goog-Api-Key": import.meta.env.VITE_GOOGLE_MAP_API_KEY,
+    "X-Goog-FieldMask": [
+      "places.name",
+      "places.displayName",
+      "places.photos",
+      "places.googleMapsUri",
+      "places.location",
+    ],
+  },
 };
 
-export const getCityDetails = (data) => {
-  const fieldMask = [
-    "name",
-    "displayName",
-    "photos",
-    "googleMapsUri",
-    "location",
-  ];
-  return axios.post(BASE_URL, data, getHeaders(fieldMask));
-};
+export const PHOTO_URL =
+  "https://places.googleapis.com/v1/{replace}/media?maxHeightPx=1000&key=" +
+  import.meta.env.VITE_GOOGLE_MAP_API_KEY;
+
+export const getPlaceDetails = (data) =>
+  axios.post(BASE_URL, data, configPlace);
+export const getCityDetails = (data) => axios.post(BASE_URL, data, configCity);
